fdca204e0bc6085203a5641e1dcdc9bdf3bbfa54
theSymbol255 <manuelrottmann@googlemail.com>
2019-10-11 11:03:44 +0200

    rotate vector

--- src/Utility.cs
+++ src/Utility.cs
@@ -10,7 +10,7 @@ namespace EvcMonogame.src
 {
     public static class Utility
     {
-        public static Vector2 scaleVector(Vector2 originalVector, float toVectorLength)
+        public static Vector2 scaleVectorTo(Vector2 originalVector, float toVectorLength)
         {
             Vector2 scaledVector = new Vector2(0, 0);
             if (!(originalVector.X == 0 && originalVector.Y == 0))
@@ -20,6 +20,16 @@ namespace EvcMonogame.src
                 scaledVector.Y = (float)(toVectorLength * Math.Sin(phi));    //  and calculates the new X and the new Y for the scaled vector
             }
             return scaledVector;
+        }
+
+        public static Vector2 rotateVectorAntiClockwise(Vector2 originalVector, float angelInDegree){
+            Vector2 rotatedVector = new Vector2(0,0);
+            float r = Math.Sqrt((originalVector.X^2)+(originalVector.Y^2));
+            float phi = Math.Atan2(originalVector.Y, originalVector.X);
+            float rotationScale = Math.PI*(1/(180/angelInDegree));
+            rotatedVector.X = (float)(r * Math.Cos(phi+rotationScale));
+            rotatedVector.Y = (float)(r * Math.Sin(phi+rotationScale));
+            return rotatedVector;
         }
     }
 }
