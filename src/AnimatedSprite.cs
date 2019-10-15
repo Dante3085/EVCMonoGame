a771215eb63aff692d1bbe334831b7e173137657
mjsch <moritz97.s@web.de>
2019-10-15 13:21:06 +0200

    AnimatedSprite: LoadFromFile() with mirrored bool

--- src/AnimatedSprite.cs
+++ src/AnimatedSprite.cs
@@ -17,9 +17,7 @@ namespace EVCMonoGame.src
     {
         struct Animation
         {
-            // public Rectangle[] frames;
-            // public int frameDelay;
-            // public bool mirrored;
+            // TODO: Offsets für frames, weil sonst Verschiebung der Figur/Ankerpunkt.
 
             public Rectangle[] Frames
             {
@@ -91,6 +89,11 @@ namespace EVCMonoGame.src
             get { return previousPosition; }
         }
 
+        public String CurrentAnimation
+        {
+            get { return currentAnimation; }
+        }
+
         #endregion
 
         public AnimatedSprite(String spritesheetName, Vector2 position, float scale = 1.0f)
@@ -137,8 +140,7 @@ namespace EVCMonoGame.src
             spriteBatch.Draw(spritesheet, position, currentAnim.Frames[frameIndex], Color.White,
                 0, Vector2.Zero, scale, currentAnim.Mirrored ? SpriteEffects.FlipHorizontally : SpriteEffects.None, 1);
 
-            // spriteBatch.DrawString(debugFont, "frame: " + frameIndex, position, Color.White);
-            spriteBatch.DrawString(debugFont, "elapsed: " + elapsedMillis, position, Color.White);
+            spriteBatch.DrawString(debugFont, "animation: " + currentAnimation, position, Color.White);
         }
 
         public void LoadContent(ContentManager content)
@@ -149,6 +151,10 @@ namespace EVCMonoGame.src
 
         public void AddAnimation(String name, Rectangle[] frames, int frameDelay, bool mirrored = false)
         {
+            if (animations.ContainsKey(name))
+            {
+                Console.WriteLine("Animation '" + name + "' has just been overriden by an Animation with the same name.");
+            }
             animations[name] = new Animation(frames, frameDelay, mirrored);
             currentAnimation = name;
         }
@@ -185,7 +191,7 @@ namespace EVCMonoGame.src
             AddAnimation(name, frames, frameDelay);
         }
 
-        public void SetAnimation(String name, bool mirrored = false)
+        public void SetAnimation(String name)
         {
             // Do nothing if the given Animation is already set.
             if (currentAnimation == name)
@@ -199,9 +205,6 @@ namespace EVCMonoGame.src
                     " the given Animation.");
             }
             currentAnimation = name;
-            Animation currentAnim = animations[currentAnimation];
-            currentAnim.Mirrored = mirrored;
-            animations[currentAnimation] = currentAnim;
             elapsedMillis = 0;
             frameIndex = 0;
         }
@@ -225,10 +228,12 @@ namespace EVCMonoGame.src
 
                     String animName = "";
                     String frameDelayMillis = "";
+                    String mirrored = "";
                     List<Rectangle> frames = new List<Rectangle>();
 
                     bool readAnimName = false;
                     bool readFrameDelay = false;
+                    bool readMirrored = false;
                     bool readFrame = false;
 
                     for (int i = 0; i < line.Length; ++i)
@@ -257,10 +262,20 @@ namespace EVCMonoGame.src
                             if (line[i] == ',')
                             {
                                 readFrameDelay = false;
-                                readFrame = true;
+                                readMirrored = true;
                                 continue;
                             }
                             frameDelayMillis += line[i];
+                        }
+                        else if (readMirrored)
+                        {
+                            if (line[i] == ',')
+                            {
+                                readMirrored = false;
+                                readFrame = true;
+                                continue;
+                            }
+                            mirrored += line[i];
                         }
                         else if (readFrame)
                         {
@@ -272,7 +287,7 @@ namespace EVCMonoGame.src
 
                         }
                     }
-                    AddAnimation(animName, frames.ToArray(), int.Parse(frameDelayMillis));
+                    AddAnimation(animName, frames.ToArray(), int.Parse(frameDelayMillis), bool.Parse(mirrored));
                 }
             }
             file.Close();
