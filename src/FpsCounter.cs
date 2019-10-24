a193ddcb5e014c5cebac3e34c9e3743ab367f90d
mjsch <moritz97.s@web.de>
2019-10-24 15:04:35 +0200

    Modified FpsCounter so that it can display fpsValues for longer(readability) and store incoming ones in a Buffer and display them later.

--- src/FpsCounter.cs
+++ src/FpsCounter.cs
@@ -19,18 +19,45 @@ namespace EVCMonoGame.src
         private Vector2 position;
         Color color;
 
+        private int delay;
+        private int elapsed;
+        private List<float> fpsBuffer;
+        private int indexCurrentFpsValue;
+
         public FpsCounter(Vector2 position, Color color)
         {
-            fps = -1;
             str = "";
             this.position = position;
             this.color = color;
+
+            delay = 500;
+            elapsed = 0;
+            fpsBuffer = new List<float>();
+            indexCurrentFpsValue = 0;
         }
 
         public override void Update(GameTime gameTime)
         {
+            elapsed += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
             fps = 1.0f / (float)gameTime.ElapsedGameTime.TotalSeconds;
-            str = fps.ToString();
+            fpsBuffer.Add(fps);
+
+            // Only display next fps value after a certain amount of time.
+            // Otherwise humans can't follow it.
+            if (elapsed >= delay)
+            {
+                // Reset elapsed
+                elapsed = 0;
+
+                // Put the current fps Value into the display string
+                str = fpsBuffer[indexCurrentFpsValue++ % fpsBuffer.Count].ToString();
+
+                // Remove all old fps values from the buffer
+                fpsBuffer.RemoveRange(0, indexCurrentFpsValue);
+
+                // Set the index for the next fps value to be displayed.
+                indexCurrentFpsValue = 0;
+            }
         }
 
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
