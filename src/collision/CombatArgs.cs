e318fe453fddda8b25930368ac5799e1e81d791a
mjsch <moritz97.s@web.de>
2019-10-29 09:26:22 +0100

    Missing CombatArgs class

---
+++ src/collision/CombatArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVCMonoGame.src.collision
+{
+    public struct CombatArgs
+    {
+        public bool knockBack;
+
+        public CombatArgs(bool knockBack = false)
+        {
+            this.knockBack = knockBack;
+        }
+    }
+}
