cc2ac806080e3df9797697f69395605b921c459b
mjsch <moritz97.s@web.de>
2019-11-02 19:46:39 +0100

    Move Tilemap to src/tilemap/

--- src/tilemap/Tilemap.cs
+++ src/tilemap/Tilemap.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 using Microsoft.Xna.Framework.Content;
 
-namespace EVCMonoGame.src
+namespace EVCMonoGame.src.tilemap
 {
     public class Tilemap : scenes.IDrawable
     {
