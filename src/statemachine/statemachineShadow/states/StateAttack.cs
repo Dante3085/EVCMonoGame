959b26af4e8ab5438d12be3178ba7b26a388813e
theSymbol255 <manuelrottmann@googlemail.com>
2019-12-03 14:44:10 +0100

    stateattack shadow

--- src/statemachine/statemachineShadow/states/StateAttack.cs
+++ src/statemachine/statemachineShadow/states/StateAttack.cs
@@ -16,21 +16,27 @@ namespace EVCMonoGame.src.statemachine.shadow
         public StateManagerShadow stateManagerShadow;
         public TimeSpan lastAttack = new TimeSpan(0, 0, 0);
         public TimeSpan cooldown = new TimeSpan(0, 0, 3);
-        public Shadow shadow; 
-        public StateAttack(StateManagerShadow stateManager, params Transition[] transitions)
+        public Shadow shadow;
+        public StateAttack(/*StateManagerShadow stateManager*/ Shadow shadow, params Transition[] transitions)
             : base("Attack", transitions)
         {
-            this.shadow = stateManager.shadow;
-            this.stateManagerShadow = stateManagerShadow;
+            this.shadow = /*stateManager.*/shadow;
+            //this.stateManagerShadow = stateManagerShadow;
         }
         public override void Enter(GameTime gameTime)
         {
             base.Enter(gameTime);
-            Player nearestPlayer = CollisionManager.GetNearestPlayerInRange(stateManagerShadow.shadow, 300);
-            shadow.Sprite.SetAnimation("NORMAL_ATTACK_LEFT");
-            if (!nearestPlayer.HurtBounds.Intersects(shadow.AttackBounds))
-            {
-                shadow.Sprite.SetAnimation("NORMAL_ATTACK_RIGHT");
+            if (CollisionManager.IsPlayerInRange(shadow, 300)) {
+                Player nearestPlayer = CollisionManager.GetNearestPlayerInRange(shadow, 10000);
+
+                if (nearestPlayer.Sprite.Bounds.Center.X > shadow.Sprite.Bounds.Center.X)
+                {
+                    shadow.Sprite.SetAnimation("NORMAL_ATTACK_RIGHT");
+                }
+                else
+                {
+                    shadow.Sprite.SetAnimation("NORMAL_ATTACK_LEFT");
+                }
             }
             //attacklogic
 
@@ -45,7 +51,7 @@ namespace EVCMonoGame.src.statemachine.shadow
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
-            CollisionManager.CheckCombatCollisions(stateManagerShadow.shadow);
+            CollisionManager.CheckCombatCollisions(shadow);
         }
     }
 }
