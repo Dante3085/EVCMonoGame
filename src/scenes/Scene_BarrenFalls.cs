9e71fb924115ce31109c58dfd3a5fe5b23a9d5b0
mjsch <moritz97.s@web.de>
2019-12-10 14:18:18 +0100

    Credits in BarrenFalls.

--- src/scenes/Scene_BarrenFalls.cs
+++ src/scenes/Scene_BarrenFalls.cs
@@ -18,8 +18,7 @@ namespace EVCMonoGame.src.scenes
 {
     public class Scene_BarrenFalls : Scene
     {
-
-       
+        private Credits credits;
 
         public Scene_BarrenFalls(SceneManager sceneManager)
             : base(sceneManager)
@@ -38,6 +37,33 @@ namespace EVCMonoGame.src.scenes
 
             camera.SetCameraToPosition(Vector2.Zero, Screenpoint.UP_LEFT_EDGE);
             camera.Zoom = 0.5f;
+
+            credits = new Credits(new Vector2(1100, -1300), sceneManager.GraphicsDevice.Viewport, 100, sceneManager, 60,
+                      new string[]
+            {
+                "Programmers",
+                "      Manuel Rottmann",
+                "      Dennis Frac",
+                "      Moritz Schaefer",
+
+                "Libraries",
+                "      MonoGame.Primitives2D",
+                "      Robert Penner's Easing Functions",
+
+                "Assets",
+                "      Final Fantasy 6 Tilesets",
+                "      Kingdom Hearts - Chain of Memories(GBA) Spritesheets",
+            });
+
+            updateables.AddRange(new IUpdateable[]
+            {
+                credits,
+            });
+
+            drawables.AddRange(new IDrawable[]
+            {
+                credits,
+            });
         }
     }
 }
