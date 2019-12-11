673632849286d9a99e43ffa752a709abad76d664
mjsch <moritz97.s@web.de>
2019-12-11 14:41:35 +0100

    Enemies in all rooms with random spawn locations.

--- src/scenes/scenes3_castle/Scene_Castle_Room4.cs
+++ src/scenes/scenes3_castle/Scene_Castle_Room4.cs
@@ -8,7 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 
 using EVCMonoGame.src.tilemap;
-using EVCMonoGame.src.characters;
+using EVCMonoGame.src.characters.enemies;
 using EVCMonoGame.src.states;
 
 
@@ -16,6 +16,7 @@ namespace EVCMonoGame.src.scenes.castle
 {
     public class Scene_Castle_Room4 : Scene
     {
+        private Shadow[] shadows;
 
         public Scene_Castle_Room4(SceneManager sceneManager)
             : base(sceneManager)
@@ -30,6 +31,45 @@ namespace EVCMonoGame.src.scenes.castle
 
             sora.WorldPosition = new Vector2(565, 6400);
             riku.WorldPosition = new Vector2(1900, 6400);
+
+            enemySpawnLocationsLeftLane.AddRange(new Vector2[]
+            {
+                new Vector2(258, 5700),
+                new Vector2(887, 5661),
+                new Vector2(275, 3372),
+                new Vector2(892, 3407),
+                new Vector2(249, 989),
+                new Vector2(879, 951),
+                new Vector2(576, 2454),
+            });
+
+            enemySpawnLocationsRightLane.AddRange(new Vector2[]
+            {
+                new Vector2(1569, 1001),
+                new Vector2(1600, 3418),
+                new Vector2(2204, 3395),
+                new Vector2(1600, 5704),
+                new Vector2(2279, 5761),
+                new Vector2(1937, 4859),
+                new Vector2(1938, 1997),
+            });
+            RandomizeEnemySpawnLocations();
+
+            shadows = new Shadow[12];
+            for (int i = 0; i < shadows.Length; ++i)
+            {
+                if (i % 2 == 0)
+                {
+                    shadows[i] = new Shadow(NextEnemySpawnLocationLeftLane());
+                }
+                else
+                {
+                    shadows[i] = new Shadow(NextEnemySpawnLocationRightLane());
+                }
+
+                updateables.Add(shadows[i]);
+                drawables.Add(shadows[i]);
+            }
 
             doorPlayerOne = new Door(new Vector2(577, 384));
             doorPlayerTwo = new Door(new Vector2(1920, 383));
